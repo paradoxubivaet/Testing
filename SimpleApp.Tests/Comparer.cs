b0ebd1253ffd03feee566c9ebc14ea688822085b
BlackBroke <paradox.ubivaet@mail.ru>
2022-06-27 20:28:12 +0800

    Добавить тест для HomeController.

---
+++ SimpleApp.Tests/Comparer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleApp.Tests
+{
+
+    public class Comparer
+    {
+        public static Comparer<U> Get<U>(Func<U, U, bool> func)
+        {
+            return new Comparer<U>(func);
+        }
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>
+    {
+        private Func<T, T, bool> comprasionFunction;
+
+        public Comparer(Func<T, T, bool> func)
+        {
+            comprasionFunction = func;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return comprasionFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
--- SimpleApp/Models/Product.cs
+++ SimpleApp/Models/Product.cs
@@ -1,25 +1,20 @@
-﻿namespace SimpleApp.Models
+﻿using System.Collections.Generic;
+
+namespace SimpleApp.Models
 {
     public class Product
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
+    }
 
-        public static Product[] GetProducts()
-        {
-            Product kayak = new Product
+    public class ProductDataSource : IDataSource
+    {
+        public IEnumerable<Product> Products =>
+            new Product[]
             {
-                Name = "Kayak",
-                Price = 275M
-            };
-
-            Product lifejacket = new Product
-            {
-                Name = "Lifejacket",
-                Price = 48.95M
-            };
-
-            return new Product[] { kayak, lifejacket };
-        }
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M }
+            }; 
     }
 }
